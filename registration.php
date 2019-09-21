538091ae484c0541fcad1a9aeedac2fb8e5ddcad
Nickolas Burr <nickolasburr@gmail.com>
2019-09-21 15:20:14 -0400

    Initial commit

---
+++ registration.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * registration.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/googlecloudstorage/LICENSE.txt
+ *
+ * @package       AuroraExtensions_GoogleCloudStorage
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'AuroraExtensions_GoogleCloudStorage',
+    __DIR__
+);
