58b6641242ffc0b2d922f48217fded71ae174896
Nickolas Burr <nickolasburr@gmail.com>
2019-09-23 15:25:29 -0400

    Add file storage model

---
+++ Model/File/Storage.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Storage.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/googlecloudstorage/LICENSE.txt
+ *
+ * @package       AuroraExtensions_GoogleCloudStorage
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\GoogleCloudStorage\Model\File;
+
+class Storage
+{
+    /** @constant int STORAGE_MEDIA_GCS */
+    public const STORAGE_MEDIA_GCS = 2;
+}
