d3794e1568e905082fdfa25dc3f2b72b1b121ab1
Nickolas Burr <nickolasburr@gmail.com>
2019-09-24 00:45:06 -0400

    Add storage bucket resource model

---
+++ Model/ResourceModel/File/Storage/Bucket.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Bucket.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/googlecloudstorage/LICENSE.txt
+ *
+ * @package       AuroraExtensions_GoogleCloudStorage
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\GoogleCloudStorage\Model\ResourceModel\File\Storage;
+
+use AuroraExtensions\GoogleCloudStorage\Api\StorageObjectManagementInterface;
+
+class Bucket
+{
+    /** @property StorageObjectManagementInterface $storageAdapter */
+    protected $storageAdapter;
+
+    /**
+     * @param StorageObjectManagementInterface $storageAdapter
+     * @return void
+     */
+    public function __construct(
+        StorageObjectManagementInterface $storageAdapter
+    ) {
+        $this->storageAdapter = $storageAdapter;
+    }
+
+    /**
+     * @param string $dirname
+     * @return void
+     */
+    public function deleteFolder(string $dirname = ''): void
+    {
+        /* Trim trailing slash from $dirname. */
+        $dirname = rtrim($dirname, '/');
+
+        if (strlen($dirname)) {
+            /* Append slash back to $dirname. */
+            $dirname .= '/';
+
+            /* Delete all objects with $dirname prefix. */
+            $this->storageAdapter->deleteAllObjects(['prefix' => $dirname]);
+        }
+    }
+}
